f1c2508b4c9fa39dc69dac3e1e93cf41359ba2c9
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2023-07-12 12:51:06 +0200

    Editorial changes only.

--- chapter-02/exercise-02-05/exercise-02-05.cpp
+++ chapter-02/exercise-02-05/exercise-02-05.cpp
@@ -1,21 +1,17 @@
 // Program to calculate the product of three integers.
-
 #include <iostream>     // Enables program to perform input and output.
-
 using namespace std;    // Program uses names from the std namespace
 
 // Function main begins program execution.
 int main()
 {
-    int factor1{0};    // First integer to multiply.
-    int factor2{0};    // Second integer to multiply
-    int factor3{0};    // Third integer to multiply
-    
+    int factor1{0};    // First integer to multiply storage.
+    int factor2{0};    // Second integer to multiply storage.
+    int factor3{0};    // Third integer to multiply storage.
     cout << "Enter three integers: ";        // Prompt user for data.
     cin >> factor1 >> factor2 >> factor3;    // Read in data from user.
     
-    int product{0};    // The product of the three integers.
-    
+    int product{0};    // The product of the three integers storage.
     product= factor1 * factor2 * factor3;           // Multiplication.
     cout << "The product is " << product << endl;   // Present the product.
 }    // Function main ends.
