1a2c1b17609fbf002ca31e04dfe2b96921466a29
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2023-07-17 00:03:25 +0200

    Fix indentation.

--- chapter-05/exercise-05-11/main.cpp
+++ chapter-05/exercise-05-11/main.cpp
@@ -3,25 +3,28 @@
 //
 // First value read is interpreted as the number of values the user will enter,
 // this must be 1 or higher. The user is then expected to enter integer values.
-// The smallest value that the user entered is presented after the user have 
+// The smallest value that the user entered is presented after the user have
 // entered the specified number of integer values.
 ////////////////////////////////////////////////////////////////////////////////
 #include <iostream>
-#include <limits> 
+#include <limits>
 
-int main() {
+int main()
+{
     int numberOfValues = 0;
     std::cin >> numberOfValues;
 
     int smallestValue;
     std::cin >> smallestValue;
-    
-    for (int i = 1; i < numberOfValues; i++) {
-      int inputValue;
-      std::cin >> inputValue;
-      if (smallestValue > inputValue) {
-        smallestValue = inputValue;
-      }
+
+    for (int i = 1; i < numberOfValues; i++)
+    {
+        int inputValue;
+        std::cin >> inputValue;
+        if (smallestValue > inputValue)
+        {
+            smallestValue = inputValue;
+        }
     }
 
     std::cout << smallestValue << "\n";
