9546a6f4f37bb855f28859b93d84791d451c83c4
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2023-07-17 13:26:45 +0200

    Add solution to exercise 5.11.

--- chapter-05/exercise-05-12/main.cpp
+++ chapter-05/exercise-05-12/main.cpp
@@ -2,10 +2,28 @@
 // Application that calculates the product of the multiple of 3
 //
 // Products that are multiple of 3 are calculated for the range 3 to 50 and
-// presented.
+// presented in the terminal window.
 ////////////////////////////////////////////////////////////////////////////////
 #include <iostream>
 
-int main() {
-    
+int main()
+{
+    int multiplicand = 3; // Will calculate multiples of this multiplicand.
+    int multiplier = 1;   // Tracks what multiplier to use for next calculation.
+    int product = 3;      // Storage for multiplication result.
+    int upper_range = 50; // The upper range limit that will end the calculation.
+
+    while (product <= upper_range)
+    {
+        // First present the result of the previous calculation.
+        std::cout << multiplicand << " x "
+                  << multiplier << " = "
+                  << product << "\n";
+
+        // Then calculate the next consecutive product.
+        ++multiplier;
+        product = multiplicand * multiplier;
+    }
+
+    return 0;
 }
